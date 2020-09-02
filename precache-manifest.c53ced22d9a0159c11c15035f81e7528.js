1467142884f4c2bd0a00559a74ceebe8fde57226
Nick Morton <nick.john.morton@gmail.com>
2020-09-02 12:42:25 +1000

    Updates

---
+++ precache-manifest.c53ced22d9a0159c11c15035f81e7528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1451e3f6ee992bdc875e9ff13f53afe",
+    "url": "/spotify-player/index.html"
+  },
+  {
+    "revision": "c018fb983da74c60d1c3",
+    "url": "/spotify-player/static/css/main.b484b375.chunk.css"
+  },
+  {
+    "revision": "29f91a4a51c49ab1daa9",
+    "url": "/spotify-player/static/js/2.b8e67975.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-player/static/js/2.b8e67975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c018fb983da74c60d1c3",
+    "url": "/spotify-player/static/js/main.4c11d91a.chunk.js"
+  },
+  {
+    "revision": "b6d71938b8adcfe0ac88",
+    "url": "/spotify-player/static/js/runtime-main.caf68c0a.js"
+  }
+]);
